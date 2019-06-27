b2d60599e58386e3c643ab39e6afb4987aeb5c6e
Melvin Mathews <mel.matt007@gmail.com>
2019-06-26 17:25:23 -0700

    update for valid dataset

--- src/solve.py
+++ src/solve.py
@@ -1,69 +1,71 @@
 from matching import MentorMentee
 
-# import csv
+import csv
 
-# tee = open('Mentee.csv', mode='r')
+tee = open('Mentee.csv', mode='r', encoding='utf-8-sig')
 
-# readertee = csv.reader(tee)
+readertee = csv.reader(tee)
 
-# menteeDict = {}
+menteeDict = {}
 
-# for row in readertee:
-#     val = []
-#     for i in range(1,5):
-#         if row[i]:
-#             val.append(row[i])
+for row in readertee:
+    val = []
+    for i in range(1,5):
+        if row[i]:
+            val.append(row[i])
 
-#     menteeDict.update({row[0]:val})
+    menteeDict.update({row[0]:val})
 
-# print(menteeDict)
+print(menteeDict)
 
-# tor = open('Mentor.csv', mode='r')
+tor = open('Mentor.csv', mode='r', encoding='utf-8-sig')
 
-# readertor = csv.reader(tor)
+readertor = csv.reader(tor)
 
-# mentorDict = {}
+mentorDict = {}
 
-# for row in readertor:
-#     val = []
-#     for i in range(2,20):
-#         if row[i]:
-#             val.append(row[i])
+for row in readertor:
+    val = []
+    for i in range(2,20):
+        if row[i]:
+            val.append(row[i])
 
-#     mentorDict.update({row[0]:val})
+    mentorDict.update({row[0]:val})
 
-# print(mentorDict)
+print(mentorDict)
 
-# cap = open('Mentor.csv', mode='r')
+cap = open('Mentor.csv', mode='r', encoding='utf-8-sig')
 
-# readercap = csv.reader(cap)
+readercap = csv.reader(cap)
 
-# capDict = {}
+capDict = {}
 
-# for row in readercap:
-#     capDict.update({row[0]:row[1]})
+for row in readercap:
+    capDict.update({row[0]:row[1]})
 
-# print(capDict)
+print(capDict)
 
 
-mentee_prefs = {
-    "747033": ["88","87","60"],
-    "747021": ["88", "87", "60"],
-    "747457": ["88", "60", "87"],
-}
+# mentee_prefs = {
+#     "747033": ["88","87","60"],
+#     "747021": ["88", "87", "60"],
+#     "747457": ["88", "60", "87"],
+# }
 
-mentor_prefs = {
-   "88": ["747033", "747021", "747457"],
-   "87": ["747033", "747021", "747457"],
-   "60": ["747033", "747457", "747021"]
-}
+# mentor_prefs = {
+#    "88": ["747033", "747021", "747457"],
+#    "87": ["747033", "747021", "747457"],
+#    "60": ["747033", "747457", "747021"]
+# }
 
-capacities = {
-    "88": 2,
-    "87": 3,
-    "60": 1
-}
+# capacities = {
+#     "88": 2,
+#     "87": 3,
+#     "60": 1
+# }
 
-game = MentorMentee.create_from_dictionaries(mentee_prefs, mentor_prefs, capacities)
+# game = MentorMentee.create_from_dictionaries(mentee_prefs, mentor_prefs, capacities)
+
+game = MentorMentee.create_from_dictionaries(menteeDict, mentorDict, capDict)
 
 print(game.solve())
